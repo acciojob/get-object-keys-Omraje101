3d10867be18761fe4864a71974602017f1523dde
Omraje101 <119411112+Omraje101@users.noreply.github.com>
2026-04-23 17:35:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,10 +3,21 @@ const student = {
   name: "John"
 };
 
-// Function to get all keys
+// Function to get all keys from the object
 function getKeys(obj) {
   return Object.keys(obj);
 }
 
-// Print result (needed for this platform)
-console.log(getKeys(student));
+// Test cases
+console.log(getKeys(student)); 
+// Output: ["name"]
+
+// Additional test
+const student2 = {
+  name: "Alice",
+  age: 20,
+  city: "Mumbai"
+};
+
+console.log(getKeys(student2)); 
+// Output: ["name", "age", "city"]
