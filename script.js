f8075302217f7a58b1a957816ee1875fbeebf3b9
Omraje101 <119411112+Omraje101@users.noreply.github.com>
2026-04-23 17:33:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//your JS code here. If required.
+// Create the student object
+const student = {
+  name: "John"
+};
+
+// Function to get all keys
+function getKeys(obj) {
+  return Object.keys(obj);
+}
+
+// Print result (needed for this platform)
+console.log(getKeys(student));
